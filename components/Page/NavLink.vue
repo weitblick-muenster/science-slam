4049f386b8576680e0c7736fcdac46b71e160e3e
Joe Pantazidis <joe.pantazidis@outlook.de>
2020-11-05 18:19:50 +0100

    add NavLink component

---
+++ components/Page/NavLink.vue
@@ -0,0 +1,7 @@
+<template>
+  <a class="no-underline block mt-4 text-primary transition-all duration-300
+    mr-4 lg:inline-block lg:mt-0 hover:shadow-primary w-fit-content"
+  >
+    <slot />
+  </a>
+</template>
