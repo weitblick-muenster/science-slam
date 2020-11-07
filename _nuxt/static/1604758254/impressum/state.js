4802a8be3c3778696e056d563bc2e058c753748f
pantajoe <joe.pantazidis@outlook.de>
2020-11-07 14:17:17 +0000

    Update gh-pages to output generated at d09b263

--- _nuxt/static/1604758254/impressum/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1604758254",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},serverRendered:false,routePath:a,config:{content:{dbHash:"8c49f464"}}}}("\u002Fimpressum"));
