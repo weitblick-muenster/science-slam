93b4c4fc3a3a1f71d01c9fc447af8a338e68ac0e
pantajoe <joe.pantazidis@outlook.de>
2020-11-07 14:00:24 +0000

    Update gh-pages to output generated at 1b2ee87

---
+++ _nuxt/static/1604757616/datenschutz/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1604757616",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},serverRendered:false,routePath:a,config:{content:{dbHash:"db63ac90"}}}}("\u002Fdatenschutz"));
