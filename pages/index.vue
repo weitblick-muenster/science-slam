de706224b4fe24e50ad98d26e8e318e21e02e4f6
Joe Pantazidis <joe.pantazidis@outlook.de>
2020-11-14 09:34:04 +0100

    show gratification message after paypal donation

--- pages/index.vue
+++ pages/index.vue
@@ -10,6 +10,8 @@
 </template>
 
 <script>
+const PAYPAL_PARAMS = ['amt', 'cc', 'cm', 'item_name', 'item_number', 'st', 'tx'];
+
 export default {
   async asyncData({ $content }) {
     const slammers = await $content('/slammers').sortBy('position', 'asc').fetch();
@@ -24,6 +26,28 @@ export default {
     return {
       streamStartsAt: '2020-11-23T19:00:00+00:00',
     };
+  },
+  mounted() {
+    const { query } = this.$route;
+
+    if (PAYPAL_PARAMS.every((param) => Object.keys(query).includes(param))) {
+      const {
+        amt: amount,
+        cc: currency,
+        st: status,
+        tx: transactionId,
+      } = query;
+
+      if (
+        currency === 'EUR'
+        && status === 'Completed'
+        && typeof transactionId === 'string'
+        && parseFloat(amount) > 0
+      ) {
+        this.$router.push({ query: {}, addToHistory: false });
+        this.$notification.success('Vielen Dank für deine Unterstützung!');
+      }
+    }
   },
 };
 </script>
